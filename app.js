91b7362e2c86f14d7ae753f786c1c72ad0e3ba83
Joseph <josephappenteng99@gmail.com>
2022-11-28 21:58:47 +0100

    app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+class App
+{
+       runApplication()
+   {let appnaam = "Java";
+    let versienummer = 0.9;
+    let versiedatum = "12-09-2022";
+    let autheur = "Joseph";
+    let copyright = "That includes such things as manuscripts, books, phonorecords, cards,"
+      "disks, film, and tapes are among some of the written materials that are considered protected under copyright law.";
+    let distributeur = "Amazon"
+    let darkmode = true
+    console.log("hello world!")
+    console.log(appnaam)
+    console.log(versienummer)
+    console.log(versiedatum)
+    console.log(autheur)
+    console.log(copyright)
+    console.log(distributeur)
+    console.log(darkmode)
+   }
+}
+
+let app = new App();
+app.runApplication();
